f31218d44219db03e09426c36030c5f8e7457cbb
Brandur <brandur@mutelight.org>
2016-02-03 14:28:28 -0800

    Fix web mocks in new country specs

--- stripe/test/resources/test_country_spec.py
+++ stripe/test/resources/test_country_spec.py
@@ -7,7 +7,7 @@ from stripe.test.helper import (
 class PlanTest(StripeResourceTest):
     def test_get_resource(self):
         stripe.CountrySpec.retrieve('US')
-        self.requestor_mock.assert_called_with(
+        self.requestor_mock.request.assert_called_with(
             'get',
             '/v1/country_specs/US',
             {},
@@ -16,9 +16,8 @@ class PlanTest(StripeResourceTest):
 
     def test_list_resource(self):
         stripe.CountrySpec.all()
-        self.requestor_mock.assert_called_with(
+        self.requestor_mock.request.assert_called_with(
             'get',
             '/v1/country_specs',
-            {},
-            None
+            {}
         )
